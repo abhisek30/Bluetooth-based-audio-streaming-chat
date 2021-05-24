d8389c15bb38d73e3a6e2f2575f26b065c26cff8
Sivnarayan Pal <shibu.bapoon100@gmail.com>
2021-05-24 15:44:59 +0530

    drawer made cyclone aya re

--- app/src/main/java/co/kit/gfg/chatapp/MainActivity.java
+++ app/src/main/java/co/kit/gfg/chatapp/MainActivity.java
@@ -1,14 +1,39 @@
 package co.kit.gfg.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout=findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView=findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+
+        NavController navController;
+        navController = Navigation.findNavController(this,R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 }
