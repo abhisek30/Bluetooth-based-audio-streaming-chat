73b60aed8e534ebb1343d09f8bc73c4907b49dd0
duttabhishek32 <duttabhishek0@gmail.com>
2021-05-25 10:16:41 +0530

    Bottom Navigation View

--- app/src/main/java/co/kit/gfg/chatapp/MainActivity.java
+++ app/src/main/java/co/kit/gfg/chatapp/MainActivity.java
@@ -1,8 +1,13 @@
 package co.kit.gfg.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Initialize the Bottom Navigation View */
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+
+        /* Nav controller */
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
